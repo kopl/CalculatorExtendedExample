261d71e40306d2a2071b8bb88052db43b8fdfd83
Christian Wende <christian.wende@devboost.de>
2015-10-13 11:04:02 +0200

    adjusted psychodelic colors to be more comfortable

--- de.kopl.demonstrator.calculator.ext/src/de/kopl/demonstrator/calculator/StyleProvider.java
+++ de.kopl.demonstrator.calculator.ext/src/de/kopl/demonstrator/calculator/StyleProvider.java
@@ -7,22 +7,22 @@ import org.eclipse.swt.widgets.Control;
 public class StyleProvider {
 
 	public void applyStlying(Calculator calculator) {
-		calculator.getMainControl().setBackground(ColorConstants.RED);
-		calculator.getMainControl().setForeground(ColorConstants.BLACK);
+		calculator.getMainControl().setBackground(ColorConstants.BLACK);
+		calculator.getMainControl().setForeground(ColorConstants.DARG_GRAY);
 
-		calculator.getMemory().setForeground(ColorConstants.YELLOW);
-		calculator.getMemory().setBackground(ColorConstants.RED);
+		calculator.getMemory().setForeground(ColorConstants.DARG_GRAY);
+		calculator.getMemory().setBackground(ColorConstants.BLACK);
 
 		calculator.getDisplay().setBackground(ColorConstants.YELLOW);
 		calculator.getDisplay().setForeground(ColorConstants.RED);
 
 		if (calculator.getStatus() != null) {
-			calculator.getStatus().setForeground(ColorConstants.WHITE);
+			calculator.getStatus().setForeground(ColorConstants.DARG_GRAY);
 		}
 
 		if (calculator.getHistory() != null) {
-			calculator.getHistory().setBackground(ColorConstants.YELLOW);
-			calculator.getHistory().setForeground(ColorConstants.RED);
+			calculator.getHistory().setBackground(ColorConstants.DARG_GRAY);
+			calculator.getHistory().setForeground(ColorConstants.WHITE);
 		}
 		
 		List<Control> children = calculator.geButtons();
